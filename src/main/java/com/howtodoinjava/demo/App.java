74ac2428b59308f0aaa1c543a1dda246014c92ce
Srinivasan Karunagaran <srinivasan_k@persistent.com>
2023-08-17 22:59:12 +0530

    new changes

---
+++ src/main/java/com/howtodoinjava/demo/App.java
@@ -0,0 +1,15 @@
+package com.howtodoinjava.demo;
+
+import java.util.Base64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
